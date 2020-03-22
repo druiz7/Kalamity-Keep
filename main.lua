534da651a4a8e0a30688915b5b3c0ee079e94883
David Ruiz <44588854+druiz7@users.noreply.github.com>
2020-03-22 18:11:55 -0500

    tower classes and main

--- main.lua
+++ main.lua
@@ -1,7 +1,14 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- David Ruiz, Nick Dea, Shuji Mizoguchi
+-- Prof Kim
 
--- Your code here
+display.setStatusBar( display.HiddenStatusBar ) 
+
+local y = display.newRect(display.contentCenterX, display.contentCenterY, 1920, 1080)
+y:setFillColor(0,1,1)
+
+local x = display.newRect(10, 1070, 1560, 900)
+x.anchorX = -1; x.anchorY=1
+
+local e = display.newRect(display.contentCenterX, display.contentCenterY, 130, 100)
+e:setFillColor(1,0,0)
+
