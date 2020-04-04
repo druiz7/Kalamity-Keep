fbd66e2b9eaef11e6f701932bf21494372dddb8b
David Ruiz <44588854+druiz7@users.noreply.github.com>
2020-04-04 00:32:39 -0500

    fixed main menu

--- scenes/Main_Menu.lua
+++ scenes/Main_Menu.lua
@@ -10,10 +10,10 @@ function scene:create( event )
     local lev1txt = display.newText("Level 1", display.contentCenterX - 400, display.contentCenterY);
     local lev2txt = display.newText("Level 2", display.contentCenterX + 400, display.contentCenterY);
     lev1:addEventListener("tap", function()
-        composer.gotoScene("Level_1", {effect = "fade", time = 250});
+        composer.gotoScene("scenes.Level_1", {effect = "fade", time = 250});
     end);
     lev2:addEventListener("tap", function()
-        composer.gotoScene("Level_2", {effect = "fade", time = 250});
+        composer.gotoScene("scenes.Level_2", {effect = "fade", time = 250});
     end);
     sceneGroup:insert(lev1);
     sceneGroup:insert(lev2);
