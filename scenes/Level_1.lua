16111715365ae57f70db268916a45855fb16161d
David Ruiz <44588854+druiz7@users.noreply.github.com>
2020-04-09 19:13:02 -0500

    cleaned up level one code

--- scenes/Level_1.lua
+++ scenes/Level_1.lua
@@ -1,5 +1,7 @@
 -- David Ruiz, Nick Dea, Shuji Mizoguchi
 -- Prof. Kim
+
+local widget = require("widget")
 local physics = require("physics")
 physics.start()
 physics.setGravity (0,0);
@@ -16,11 +18,58 @@ local lizard = require("enemies.lizard")
 local troll = require("enemies.troll")
 
 local scene = composer.newScene()
+local sceneGroup
 
-function scene:create( event )
-    local sceneGroup = self.view
+local zone
+local logArr
 
-    -- sets up fill
+local gold
+local goldTxt
+
+local unitType = "";
+local function zoneHandler(event)
+    local zone = event.target
+    local clickPosX = 0;
+    local clickPosY = 0;
+    clickPosX, clickPosY = zone:contentToLocal(event.x, event.y);
+
+    --Converts the click into the indicies for a 2D array 
+    clickPosX = clickPosX + 780;  
+    clickPosY = clickPosY + 450;  
+    clickPosX = math.ceil( clickPosX/130 );
+    clickPosY = math.ceil( clickPosY/100 );
+
+    --print(clickPosX);
+    --print(clickPosY);
+
+    --Converts the clickPos coordinates back into x,y coordinates
+    local _x, _y = zone:localToContent(-715 + 130*(clickPosX-1), -400 + 100*(clickPosY-1))
+
+    if(unitType == "Wizard" and logArr[clickPosX][clickPosY] == 0)then
+        local wizard = Wizard:new({posX = _x, posY = _y})
+        wizard:spawn()
+        logArr[clickPosX][clickPosY] = 1;
+        unitType = "";
+        zone:removeEventListener("tap", zoneHandler);
+    end
+    if(unitType == "Knight" and logArr[clickPosX][clickPosY] == 0)then
+        local knight = Knight:new({posX = _x, posY = _y})
+        knight:spawn()
+        logArr[clickPosX][clickPosY] = 1;
+        unitType = "";
+        zone:removeEventListener("tap", zoneHandler);
+    end
+    if(unitType == "Archer" and logArr[clickPosX][clickPosY] == 0)then
+        local archer = Archer:new({posX = _x, posY = _y})
+        archer:spawn()
+        logArr[clickPosX][clickPosY] = 1;
+        unitType = "";
+        zone:removeEventListener("tap", zoneHandler);
+    end
+end
+
+local function createBg()
+     -- sets up fill
     display.setDefault( "textureWrapX", "repeat" )
     display.setDefault( "textureWrapY", "mirroredRepeat" )
 
@@ -32,7 +81,7 @@ function scene:create( event )
     sceneGroup:insert(bg)
 
     --Creates the playable area
-    local zone = display.newRect(10, 170, 1560, 900)
+    zone = display.newRect(10, 170, 1560, 900)
     zone.anchorX = 0; zone.anchorY=0
     zone.strokeWidth = 4;
     zone.fill = {type="image", filename="chars/tiles/grass.png"}
@@ -43,10 +92,8 @@ function scene:create( event )
     sceneGroup:insert(zone)
 
     local Grid = display.newGroup()
-
     local vert = 10;
     local vGridlines = display.newGroup();
-
     local horiz = 170;
     local hGridlines = display.newGroup();
 
@@ -70,154 +117,11 @@ function scene:create( event )
         hGridlines:insert(horizGrid);
     end
 
-    --Creates a 2D array used for logic within the game
-    logArr = {}
-    for L=1, 12 do
-        logArr[L] = {}
-        for H=1, 9 do
-            logArr[L][H] = 0;
-        end
-    end
-
     Grid:insert(vGridlines);
     Grid:insert(hGridlines);
     sceneGroup:insert(Grid);
-    
-    --Creates the gold count
-    local gold = 500;
-    local goldTxt = display.newText("Gold: " .. gold, 60, 60);
-    sceneGroup:insert(goldTxt);
-    goldTxt.anchorX = 0; goldTxt.anchorY = 0;
-    goldTxt:setFillColor(0,0,0);
-
-    --Creates the buy menu display objects
-    local wizBuy = display.newRect( 1740, 320, 340, 300);
-    local wizBuyTxt = display.newText("Wizard: 150g",1740, 320);
-    sceneGroup:insert(wizBuy);
-    sceneGroup:insert(wizBuyTxt);
-    wizBuyTxt:setFillColor(0,0,0);
-    wizBuy:setStrokeColor(0,0,0);
-    wizBuy.strokeWidth = 4;
-
-    local kniBuy = display.newRect( 1740, 620, 340, 300);
-    local kniBuyTxt = display.newText("Knight: 50g",1740, 620);
-    sceneGroup:insert(kniBuy);
-    sceneGroup:insert(kniBuyTxt);
-    kniBuyTxt:setFillColor(0,0,0);
-    kniBuy:setStrokeColor(0,0,0);
-    kniBuy.strokeWidth = 4;
-
-    local arcBuy = display.newRect( 1740, 920, 340, 300);
-    local arcBuyTxt = display.newText("Archer: 100g",1740, 920);
-    sceneGroup:insert(arcBuy);
-    sceneGroup:insert(arcBuyTxt);
-    arcBuyTxt:setFillColor(0,0,0);
-    arcBuy:setStrokeColor(0,0,0);
-    arcBuy.strokeWidth = 4;
-
-    local clickPosX;
-    local clickPosY;
-    local unitType = "";
-
---[[local barbarian = barbarian:new({xSpawn = display.contentCenterX, ySpawn = display.contentCenterY})
-    barbarian:spawn()
-    local lizard = lizard:new({xSpawn = display.contentCenterX-50, ySpawn = display.contentCenterY})
-    lizard:spawn()
-    local troll = troll:new({xSpawn = display.contentCenterX-100, ySpawn = display.contentCenterY})
-    troll:spawn()
-
-    local enemy = display.newRect(display.contentCenterX + 300, display.contentCenterY, 150, 150)
-    enemy.tag = "enemy"
-    enemy:setFillColor(1,1,0)
-    physics.addBody(enemy, "static") ]]
-
-
-    local function unitPlacement(event)
-        clickPosX = 0;
-        clickPosY = 0;
-        clickPosX, clickPosY = event.target:contentToLocal(event.x, event.y);
-
-        --Converts the click into the indicies for a 2D array 
-        clickPosX = clickPosX + 780;  
-        clickPosY = clickPosY + 450;  
-        clickPosX = math.ceil( clickPosX/130 );
-        clickPosY = math.ceil( clickPosY/100 );
-
-        --print(clickPosX);
-        --print(clickPosY);
-
-        --Converts the clickPos coordinates back into x,y coordinates
-        local _x, _y = zone:localToContent(-715 + 130*(clickPosX-1), -400 + 100*(clickPosY-1))
-
-        if(unitType == "Wizard" and logArr[clickPosX][clickPosY] == 0)then
-            local wizard = Wizard:new({posX = _x, posY = _y})
-            wizard:spawn()
-            logArr[clickPosX][clickPosY] = 1;
-            unitType = "";
-            zone:removeEventListener("tap", unitPlacement);
-        end
-        if(unitType == "Knight" and logArr[clickPosX][clickPosY] == 0)then
-            local knight = Knight:new({posX = _x, posY = _y})
-            knight:spawn()
-            logArr[clickPosX][clickPosY] = 1;
-            unitType = "";
-            zone:removeEventListener("tap", unitPlacement);
-        end
-        if(unitType == "Archer" and logArr[clickPosX][clickPosY] == 0)then
-            local archer = Archer:new({posX = _x, posY = _y})
-            archer:spawn()
-            logArr[clickPosX][clickPosY] = 1;
-            unitType = "";
-            zone:removeEventListener("tap", unitPlacement);
-        end
-    end
-
-    --Buy button listeners
-    wizBuy:addEventListener("tap", function()
-        if(unitType ~= "") then
-            return;
-        end
-        if(gold >= 150)then
-            gold = gold - 150;
-            goldTxt.text = "Gold: " .. gold;
-            unitType = "Wizard";
-            zone:addEventListener("tap", unitPlacement);
-        end
-    end);
-    kniBuy:addEventListener("tap", function()
-        if(unitType ~= "") then
-            return;
-        end
-        if(gold >= 50)then
-            gold = gold - 50;
-            goldTxt.text = "Gold: " .. gold;
-            unitType = "Knight";
-            zone:addEventListener("tap", unitPlacement);
-        end
-    end);
-    arcBuy:addEventListener("tap", function()
-        if(unitType ~= "") then
-            return;
-        end
-        if(gold >= 100)then
-            gold = gold - 100;
-            goldTxt.text = "Gold: " .. gold;
-            unitType = "Archer";
-            zone:addEventListener("tap", unitPlacement);
-        end
-    end);
-
-    sceneGroup:insert(wizBuy);
-    sceneGroup:insert(wizBuyTxt);
-
-    sceneGroup:insert(kniBuy);
-    sceneGroup:insert(kniBuyTxt);
-
-    sceneGroup:insert(arcBuy);
-    sceneGroup:insert(arcBuyTxt);
 
     --Creates the castle
-
     local castle = display.newRect(1440, 470, 130, 300)
     castle.anchorX = 0; castle.anchorY = 0;
     castle.fill = {type="image", filename="chars/tiles/wood.png"}
@@ -228,12 +132,7 @@ function scene:create( event )
     sceneGroup:insert(castle);
     castle:toFront();
 
-    for i=4, 6 do
-        logArr[12][i] = 2;
-    end
- 
     --Creates the path
-
     local verticies = {-715,300, -195,300, -195,100, -455,100, -455,-200, -325,-200, -325,-300, -195,-300, 
                         -195,-400, 455,-400, 455,-100, 325,-100, 325,300, 455,300, 455,0, 715,0,
                         715,100, 585,100, 585,400, 195,400, 195,-200, 325,-200, 325,-300, -65,-300, 
@@ -245,6 +144,130 @@ function scene:create( event )
     path:setStrokeColor(0,0,0);
     path.strokeWidth = 4;
     sceneGroup:insert(path);
+
+    -- returns fill to default
+    display.setDefault( "textureWrapX", "clampToEdge" )
+    display.setDefault( "textureWrapY", "clampToEdge" )
+end
+
+local function createTowerBtns()
+    local towerAtr = {
+        Wizard = {cost = 150},
+        Knight = {cost = 50},
+        Archer = {cost = 100}
+    }
+
+    local function handleButtonEvent (event)
+        if unitType ~= "" then return end
+
+        local tower = event.target.id;
+        local towerCost = towerAtr[tower].cost
+        if(gold >= towerCost) then
+            gold = gold - towerCost
+            goldTxt.text = "Gold: " .. gold
+            unitType = tower
+
+            zone:addEventListener("tap", zoneHandler)
+        end
+    end
+
+    local wizBtn = widget.newButton(
+        {
+            id = "Wizard",
+            x = 1740,
+            y = 320,
+            width = 340,
+            height = 300,
+            defaultFile = "chars/buttons/default.png",
+            overFile = "chars/buttons/after.png",
+            labelColor = {default={0,0,0}, over={1,1,1}},
+            fontSize = 45,
+            label = "Wizard: 150g",
+            onRelease = handleButtonEvent
+        }
+    )
+    sceneGroup:insert(wizBtn)
+
+    local kniBtn = widget.newButton(
+        {
+            id = "Knight",
+            x = 1740,
+            y = 620,
+            width = 340,
+            height = 300,
+            defaultFile = "chars/buttons/default.png",
+            overFile = "chars/buttons/after.png",
+            labelColor = {default={0,0,0}, over={1,1,1}},
+            fontSize = 45,
+            label = "Knight: 50g",
+            onRelease = handleButtonEvent
+        }
+    )
+    sceneGroup:insert(kniBtn)
+
+    local arcBtn = widget.newButton(
+        {
+            id = "Archer",
+            x = 1740,
+            y = 920,
+            width = 340,
+            height = 300,
+            defaultFile = "chars/buttons/default.png",
+            overFile = "chars/buttons/after.png",
+            labelColor = {default={0,0,0}, over={1,1,1}},
+            fontSize = 45,
+            label = "Archer: 100g",
+            onRelease = handleButtonEvent
+        }
+    )
+    sceneGroup:insert(arcBtn)
+end
+
+local function createMenuBtns()
+    -- button uses an event to clear the game
+    local clearGame = display.newRect(1920 - 100, 100, 150, 100)
+    clearGame:addEventListener("tap", function() 
+        Runtime:dispatchEvent({name="clearGame"})
+    end)
+    sceneGroup:insert(clearGame)
+    local cg_text = display.newText("Clear", 1920-100, 100)
+    cg_text:setFillColor(1,0,0)
+    sceneGroup:insert(cg_text)
+
+    -- button pauses the game
+    local pg_text = display.newText("Pause", 1920-300, 100)
+    pg_text:setFillColor(1,0,0)
+    sceneGroup:insert(pg_text)
+    local pauseGame = display.newRect(1920 - 300, 100, 150, 100)
+    pauseGame:addEventListener("tap", function()
+        if(pg_text.text == "Pause") then
+            print("clicks")
+            Runtime:dispatchEvent({name="pauseGame"})
+            physics.pause()
+            pg_text.text = "Resume"
+        else
+            Runtime:dispatchEvent({name="resumeGame"})
+            physics.start()
+            pg_text.text = "Pause"
+        end
+    end)
+    sceneGroup:insert(pauseGame)
+    pg_text:toFront()
+end
+
+local function setUpLogArray()
+    --Creates a 2D array used for logic within the game
+    logArr = {}
+    for L=1, 12 do
+        logArr[L] = {}
+        for H=1, 9 do
+            logArr[L][H] = 0;
+        end
+    end
+
+    for i=4, 6 do
+        logArr[12][i] = 2;
+    end
 
     --Sets the path in the logical array
     for i = 1, 5 do 
@@ -276,38 +299,22 @@ function scene:create( event )
         logArr[10][i] = -1;
     end
     logArr[11][5] = -1;
+end
 
-    -- button uses an event to clear the game
-    local clearGame = display.newRect(1920 - 100, 100, 150, 100)
-    clearGame:addEventListener("tap", function() 
-        Runtime:dispatchEvent({name="clearGame"})
-    end)
-    sceneGroup:insert(clearGame)
-    local cg_text = display.newText("Clear", 1920-100, 100)
-    cg_text:setFillColor(1,0,0)
-    sceneGroup:insert(cg_text)
+function scene:create( event )
+    sceneGroup = self.view
 
-    -- button pauses the game
-    local pg_text = display.newText("Pause", 1920-300, 100)
-    pg_text:setFillColor(1,0,0)
-    sceneGroup:insert(pg_text)
-    local pauseGame = display.newRect(1920 - 300, 100, 150, 100)
-    pauseGame:addEventListener("tap", function()
-        if(pg_text.text == "Pause") then
-            print("clicks")
-            Runtime:dispatchEvent({name="pauseGame"})
-            physics.pause()
-            pg_text.text = "Resume"
-        else
-            Runtime:dispatchEvent({name="resumeGame"})
-            physics.start()
-            pg_text.text = "Pause"
-        end
-    end)
-    sceneGroup:insert(pauseGame)
-    pg_text:toFront()
+    createBg()
+    createTowerBtns()
+    createMenuBtns()
+    setUpLogArray()
 
-
+    --Creates the gold count
+    gold = 500;
+    goldTxt = display.newText("Gold: " .. gold, 60, 60);
+    sceneGroup:insert(goldTxt);
+    goldTxt.anchorX = 0; goldTxt.anchorY = 0;
+    goldTxt:setFillColor(0,0,0);
 end
 
 scene:addEventListener("create", scene)
