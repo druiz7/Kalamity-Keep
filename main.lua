b2b7addad3970c8509567b35b596249db5494b78
David Ruiz <44588854+druiz7@users.noreply.github.com>
2020-04-15 15:04:25 -0500

    fixed proj not tracking

--- main.lua
+++ main.lua
@@ -5,4 +5,4 @@ local composer = require("composer")
 
 display.setStatusBar( display.HiddenStatusBar ) 
 
-composer.gotoScene("scenes.level", {effect = "fade", time = 250, params={level="level1"}})
+composer.gotoScene("scenes.main_menu", {effect = "fade", time = 250, params={level="level1"}})
