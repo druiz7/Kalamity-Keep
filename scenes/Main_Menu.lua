e4e41500943aa7835ebc653bfa498d1410c10d31
Nick Dea <nickdea6@gmail.com>
2020-04-17 21:43:04 -0500

    Updated main menu

--- scenes/Main_Menu.lua
+++ scenes/Main_Menu.lua
@@ -18,28 +18,28 @@ function scene:create( event )
     local lev1 = display.newRect( display.contentCenterX - 500, display.contentCenterY + 80, 380, 100);
     local lev1txt = display.newText("Level 1", display.contentCenterX - 500, display.contentCenterY + 80);
     lev1txt:setFillColor(160/256,33/256,36/256)
-    lev1:setFillColor(0,0,0,0);
+    lev1:setFillColor(0,0,0,1);
     lev1:setStrokeColor(160/256,33/256,36/256);
     lev1.strokeWidth = 4;
 
     local lev2 = display.newRect( display.contentCenterX + 500, display.contentCenterY + 80, 380, 100);
     local lev2txt = display.newText("Level 2", display.contentCenterX + 500, display.contentCenterY + 80);
     lev2txt:setFillColor(160/256,33/256,36/256)
-    lev2:setFillColor(0,0,0,0);
+    lev2:setFillColor(0,0,0,1);
     lev2:setStrokeColor(160/256,33/256,36/256);
     lev2.strokeWidth = 4;
 
     local lore = display.newRect( display.contentCenterX, display.contentCenterY + 80, 380, 100);
     local loretxt = display.newText("Lore", display.contentCenterX, display.contentCenterY + 80);
     loretxt:setFillColor(160/256,33/256,36/256)
-    lore:setFillColor(0,0,0,0);
+    lore:setFillColor(0,0,0,1);
     lore:setStrokeColor(160/256,33/256,36/256);
     lore.strokeWidth = 4;
 
     local exitButton = display.newRect( display.contentCenterX, display.contentCenterY + 500, 365, 80);
     local exitText = display.newText("Exit", display.contentCenterX, display.contentCenterY + 500);
     exitText:setFillColor(160/256,33/256,36/256)
-    exitButton:setFillColor(0,0,0,0);
+    exitButton:setFillColor(0,0,0,1);
     exitButton:setStrokeColor(160/256,33/256,36/256);
     exitButton.strokeWidth = 4;
 
@@ -57,6 +57,7 @@ function scene:create( event )
     end);
 
     sceneGroup:insert(background);
+    sceneGroup:insert(title);
 
     sceneGroup:insert(lev1);
     sceneGroup:insert(lev1txt);
