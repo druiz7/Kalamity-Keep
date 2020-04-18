dddac818048f0ddfc63bf3abf90cd40124d92262
Nick Dea <nickdea6@gmail.com>
2020-04-17 20:19:56 -0500

    Updated the main menu

--- scenes/Main_Menu.lua
+++ scenes/Main_Menu.lua
@@ -5,23 +5,42 @@ local scene = composer.newScene()
 function scene:create( event )
  
     local sceneGroup = self.view
-    
-    local lev1 = display.newRect( display.contentCenterX - 400, display.contentCenterY, 480, 240);
-    local lev1txt = display.newText("Level 1", display.contentCenterX - 400, display.contentCenterY);
-    lev1txt:setFillColor(0,0,0)
-    lev1:setStrokeColor(1,0,0);
+
+    local title = display.newText("Kalamity Keep", display.contentCenterX, display.contentCenterY - 365, "assets/AquilineTwo.ttf", 188)
+    title:setFillColor(160/256,33/256,36/256)
+    title:toFront();
+
+    local background = display.newRect(0, 0, 1920, 1080);
+    background.anchorX = 0; background.anchorY = 0;
+    background.fill = {type = "image", filename = "assets/Kalamity_Keep_bg.png"};
+    background:toBack();
+
+    local lev1 = display.newRect( display.contentCenterX - 500, display.contentCenterY + 80, 380, 100);
+    local lev1txt = display.newText("Level 1", display.contentCenterX - 500, display.contentCenterY + 80);
+    lev1txt:setFillColor(160/256,33/256,36/256)
+    lev1:setFillColor(0,0,0,0);
+    lev1:setStrokeColor(160/256,33/256,36/256);
     lev1.strokeWidth = 4;
 
-    local lev2 = display.newRect( display.contentCenterX + 400, display.contentCenterY, 480, 240);
-    local lev2txt = display.newText("Level 2", display.contentCenterX + 400, display.contentCenterY);
-    lev2txt:setFillColor(0,0,0)
-    lev2:setStrokeColor(1,0,0);
+    local lev2 = display.newRect( display.contentCenterX + 500, display.contentCenterY + 80, 380, 100);
+    local lev2txt = display.newText("Level 2", display.contentCenterX + 500, display.contentCenterY + 80);
+    lev2txt:setFillColor(160/256,33/256,36/256)
+    lev2:setFillColor(0,0,0,0);
+    lev2:setStrokeColor(160/256,33/256,36/256);
     lev2.strokeWidth = 4;
 
-    local exitButton = display.newRect( display.contentCenterX, display.contentCenterY + 300, 360, 120);
-    local exitText = display.newText("Exit", display.contentCenterX, display.contentCenterY + 300);
-    exitText:setFillColor(0,0,0)
-    exitButton:setStrokeColor(1,0,0);
+    local lore = display.newRect( display.contentCenterX, display.contentCenterY + 80, 380, 100);
+    local loretxt = display.newText("Lore", display.contentCenterX, display.contentCenterY + 80);
+    loretxt:setFillColor(160/256,33/256,36/256)
+    lore:setFillColor(0,0,0,0);
+    lore:setStrokeColor(160/256,33/256,36/256);
+    lore.strokeWidth = 4;
+
+    local exitButton = display.newRect( display.contentCenterX, display.contentCenterY + 500, 365, 80);
+    local exitText = display.newText("Exit", display.contentCenterX, display.contentCenterY + 500);
+    exitText:setFillColor(160/256,33/256,36/256)
+    exitButton:setFillColor(0,0,0,0);
+    exitButton:setStrokeColor(160/256,33/256,36/256);
     exitButton.strokeWidth = 4;
 
     lev1:addEventListener("tap", function()
@@ -30,15 +49,23 @@ function scene:create( event )
     lev2:addEventListener("tap", function()
         composer.gotoScene("scenes.level", {effect = "fade", time = 250, params = {level="level2"}});
     end);
+    lore:addEventListener("tap", function()
+        composer.gotoScene("scenes.lore", {effect = "fade", time = 250});
+    end);
     exitButton:addEventListener("tap", function()
         native.requestExit();
     end);
+
+    sceneGroup:insert(background);
 
     sceneGroup:insert(lev1);
     sceneGroup:insert(lev1txt);
 
     sceneGroup:insert(lev2);
     sceneGroup:insert(lev2txt);
+
+    sceneGroup:insert(lore);
+    sceneGroup:insert(loretxt);
 
     sceneGroup:insert(exitButton);
     sceneGroup:insert(exitText);
